c0d206ae0212d8b69efc0b016e95bcd5858b3770
Kevin Cathme <kevin.cathme@udla.edu.ec>
2025-06-12 17:03:24 -0500

    main terminado grafica circulo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import figurasGeometricas.Circulo;
 import figurasGeometricas.CirculoDibujable;
-
+import javax.swing.*;
+import java.awt.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,5 +9,18 @@ public class Main {
         figurasGeometricas.CirculoDibujable circulo= new CirculoDibujable(100,0,0);
         System.out.println("El perimetro del circulo es: "+ circulo.getPerimetro());
         System.out.println("El perimetro del circulo es: "+ circulo.getArea());
+        JFrame frame= new JFrame("Dibujar Circulo");
+        JPanel panel= new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                circulo.dibujar2D(g);
+            }
+        };
+        panel.setPreferredSize(new Dimension(400,400));
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
