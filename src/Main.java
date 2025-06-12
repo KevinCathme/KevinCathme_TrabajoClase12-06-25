601a09b5327f35cc3ecf03956e049af20a41bdc2
Kevin Cathme <kevin.cathme@udla.edu.ec>
2025-06-12 16:50:19 -0500

    Main area y perimetro

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import figurasGeometricas.Circulo;
+import figurasGeometricas.CirculoDibujable;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        figurasGeometricas.CirculoDibujable circulo= new CirculoDibujable(100,0,0);
+        System.out.println("El perimetro del circulo es: "+ circulo.getPerimetro());
+        System.out.println("El perimetro del circulo es: "+ circulo.getArea());
     }
 }
