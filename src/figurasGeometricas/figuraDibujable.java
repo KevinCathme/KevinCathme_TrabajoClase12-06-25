a1889aa1dce79807f67797f84c6776fcde1da902
Kevin Cathme <kevin.cathme@udla.edu.ec>
2025-06-12 16:41:51 -0500

    crear circulo dibujable

--- src/figurasGeometricas/figuraDibujable.java
+++ src/figurasGeometricas/figuraDibujable.java
@@ -2,5 +2,5 @@ package figurasGeometricas;
 import java.awt.*;
 public interface figuraDibujable {
     public void setCoordenadas(int x, int y);
-    public void dibujar20(Graphics g);
+    public void dibujar2D(Graphics g);
 }
